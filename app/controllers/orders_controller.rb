866cfec7679edfd37befa4e056abe6c2588100e0
Aaron Jacobs <aaron.jacobs@sial.com>
2020-07-17 00:35:16 -0500

    Pagination, sorting, and searching.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,13 @@ class OrdersController < ApplicationController
   before_action :find_order!, only: :show
 
   def index
-    @orders = Order.order(:number).page params[:page]
+    @search = Order.ransack(params[:q])
+    # make name the default sort column
+    @search.sorts = 'number' if @search.sorts.empty?
+    @orders = @search.result
+                  .includes(:user)
+                  .page params[:page]
+    @search.build_condition
   end
 
   def show
